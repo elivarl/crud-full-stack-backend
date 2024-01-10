92b88bff0ec547d428b68fb76e569a2027209b1f
Elivar Largo <elivar.largo@ceiba.com.co>
2024-01-10 15:54:22 -0500

    Creación del proyecto

---
+++ src/main/java/com/icodeap/crudfullstackangular/CrudFullstackAngularApplication.java
@@ -0,0 +1,13 @@
+package com.icodeap.crudfullstackangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFullstackAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFullstackAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icodeap/crudfullstackangular/CrudFullstackAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icodeap.crudfullstackangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullstackAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
